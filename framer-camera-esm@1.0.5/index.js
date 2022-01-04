26be1d5f6748a61aaa60fc794df32bef1942b61f
deploy <deploy@users.noreply.github.com>
2022-01-04 13:47:27 +0000

    Updated

---
+++ framer-camera-esm@1.0.5/index.js
@@ -0,0 +1,11 @@
+// src/index.tsx
+import { Button } from "./Button.js";
+import { Battery } from "./Battery.js";
+import { MotionButton } from "./MotionButton.js";
+export * from "./Camera/Lens.js";
+export * from "./Camera/View.js";
+export {
+  Battery,
+  Button,
+  MotionButton
+};
